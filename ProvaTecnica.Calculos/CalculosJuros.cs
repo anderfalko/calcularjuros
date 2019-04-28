bb122a821f6a24b345bd3d13e66cac4836e06f77
Anderson Falkowski <anderfalko@gmail.com>
2019-04-28 11:51:50 -0300

    Resolvido calculo de juros

--- ProvaTecnica.Calculos/CalculosJuros.cs
+++ ProvaTecnica.Calculos/CalculosJuros.cs
@@ -1,11 +1,18 @@
-﻿namespace ProvaTecnica.Calculos
+﻿using System;
+
+namespace ProvaTecnica.Calculos
 {
     public class CalculosJuros
     {
         public double CalcurarJuros(double valorinicial, int meses)
         {
             // Fórmula: Valor Final = Valor Inicial * (1 + juros) ^ Tempo
-            return 0;
+            // Fórmula: Valor Final = Valor Inicial * (1 + juros) ^ Tempo
+            double juros = 1 + 0.01;
+            double potencializacao = Math.Pow(juros, meses);
+            double valor_final = valorinicial * potencializacao;
+            double valor_truncado = Math.Truncate(100 * valor_final) / 100;
+            return valor_truncado;
         }
     }
 }
