1fcf5d7b5cb7c9add5a002e781e37b4cafa5b5f8
Anderson Falkowski <anderfalko@gmail.com>
2019-04-28 11:31:42 -0300

    Criado projeto de classes com o calculo

---
+++ ProvaTecnica.Calculos/CalculosJuros.cs
@@ -0,0 +1,11 @@
+﻿namespace ProvaTecnica.Calculos
+{
+    public class CalculosJuros
+    {
+        public double CalcurarJuros(double valorinicial, int meses)
+        {
+            // Fórmula: Valor Final = Valor Inicial * (1 + juros) ^ Tempo
+            return 0;
+        }
+    }
+}
