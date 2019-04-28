3c7aa9a95f29821e73fad00a27dd9ac76349c3e0
Anderson Falkowski <anderfalko@gmail.com>
2019-04-28 12:04:25 -0300

    adicionada API

---
+++ ProvaTecnica.API/Controllers/CalculosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProvaTecnica.Calculos;
+
+namespace ProvaTecnica.API.Controllers
+{
+    [ApiController]
+    public class CalculosController : ControllerBase
+    {
+        /// <summary>
+        /// Calcula os Juros
+        /// </summary>
+        /// <param name="valorinicial">Valor inicial</param>
+        /// <param name="meses">Quantidade de meses</param>
+        /// <returns></returns>
+        [HttpGet("api/[action]")]
+        public double calculajuros(double valorinicial, int meses)
+        {
+            CalculosJuros calc = new CalculosJuros();
+            double valor = calc.CalcurarJuros(valorinicial, meses);
+            return valor;
+        }
+
+        /// <summary>
+        /// Redireciona para o código-fonte do projeto
+        /// </summary>
+        [HttpGet("api/[action]")]
+        public void showmethecode()
+        {
+            Response.Redirect("https://github.com/anderfalko/calcularjuros");
+        }
+    }
+}
---
+++ ProvaTecnica.API/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ProvaTecnica.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ProvaTecnica", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            //Ativa o Swagger
+            app.UseSwagger();
+
+            // Ativa o Swagger UI
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
+                //opt.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
